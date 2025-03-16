d1976634bff7dc321d29f233445aa3b64b0567e9
MiguelMmr <miguelmateosruiz@gmail.com>
2025-03-16 12:21:58 +0100

    ejercicios

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -5,21 +5,66 @@ https://www.youtube.com/live/euVbF1eatEY?si=nHS3QznAlA_C39NK&t=808
 */
 
 // 1. Escribe un comentario en una línea
+//comentario
 
 // 2. Escribe un comentario en varias líneas
+/*
+comentario
+varias lineas
+*/
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+let string = 'Esto es una string'
+let number = 2
+let bolean = true
+let undefined 
+let nullValue = null
+
 
 // 4. Imprime por consola el valor de todas las variables
-
+console.log(string)
+console.log(number)
+console.log(bolean)
+console.log(undefined)
+console.log(nullValue)
 // 5. Imprime por consola el tipo de todas las variables
+console.log(typeof(string))
+console.log(typeof(number))
+console.log(typeof(bolean))
+console.log(typeof(undefined))
+console.log(typeof(nullValue))
 
-// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipoç
+string = 'Esto es otra string'
+number = 3
+bolean = false
+undefined 
+nullValue = null
+
 
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+string = 2
+number = 'string'
+bolean = 'string'
+undefined = 5
+nullValue = 2
+
 
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+const string2 = 'string'
+const number12 = 5
+const bolean2 = False
+const undefined2 = undefined
+const nullValue2 = null
+
 
 // 9. A continuación, modifica los valores de las constantes
+ /*string2 = 'stgdgasdfring'
+ number12 = 4
+ bolean2 = True
+ undefined2 = 3
+ nullValue2 = 2*/
+
+
 
 // 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
