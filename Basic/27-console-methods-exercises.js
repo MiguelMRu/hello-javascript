e7f5158b3d28482cfa1d823e0c9018c3482d52f7
MiguelMmr <miguelmateosruiz@gmail.com>
2025-04-21 09:08:53 +0200

    ejercicos de consola

--- Basic/27-console-methods-exercises.js
+++ Basic/27-console-methods-exercises.js
@@ -3,23 +3,67 @@ Clase 7 en vídeo | 21/08/2024
 Console y módulos
 https://www.youtube.com/live/PAnxhBE5kIE?si=V0F_NsKO9lmhhatu&t=555
 */
-
+console.time("Tiempo de ejecución 1")
 // 1. Crea un función que utilice error correctamente
 
+function error1(){
+    console.error("Este es un mensaje de error: ", new Error("Ha fallado la ejecucion"))
+
+}
+
+error1()
 // 2. Crea una función que utilice warn correctamente
+
+function warn(){
+    console.warn("Este es una advertencia")
+    console.trace("Trace de la ejecucion")
+}
+
+warn()
 
 // 3. Crea una función que utilice info correctamente
 
+console.info("Este es un mensaje de información adicional.")
+
 // 4. Utiliza table
+
+let table = [
+    {name: 'Miguel', age: 26},
+    {name: 'Dante', age: '6 meses'},
+    {name: 'Alba', age: 23},
+]
+
+console.table(table)
 
 // 5. Utiliza group
 
+console.group("Usuario:")
+console.log("Nombre: Brais")
+console.log("Edad: 37")
+console.groupEnd()
+
+
 // 6. Utiliza time
+
+
 
 // 7. Valida con assert si un número es positivo
 
+let number = -1
+console.assert(number > 0, "El numero debe ser positivo")
+
 // 8. Utiliza count
+
+console.count("Click")
+console.count("Click")
+console.count("Click")
+console.countReset("Click")
+console.count("Click")
 
 // 9. Utiliza trace
 
 // 10. Utiliza clear
+
+//console.clear()
+
+console.timeEnd("Tiempo de ejecución 1")
